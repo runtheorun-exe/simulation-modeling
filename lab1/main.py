e5e605e221946c5da7b3fe72638d6bbf98554858
Theo P Charalampidis <runtheorun3@gmail.com>
2021-10-25 10:42:33 +0300

    Corrected lab1;Initiated lab2; pending modules to be written

--- lab1/main.py
+++ lab1/main.py
@@ -1,7 +1,6 @@
 from random import randint
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
 
 n = 1000
 nodes = [0] * n
@@ -15,32 +14,24 @@ x=1
 
 nodes[randint(0,n-1)] = 1
 
-while infectionDone == False:
-        for i in range(n):
-            if nodes[i] == 1:
-                target = randint(0,n-1)
-                if nodes[target] == 0:
-                    nodes[target] = 1
-                    infectedPerLoop+=1
-            currentInfected += infectedPerLoop
-            infectionRate.append (currentInfected)
-            curInf.append(currentInfected)
-            attempts.append(x)
-            x+=1
-            infectedPerLoop = 0
-
-        if currentInfected == n:infectionDone = True 
-            
+while not infectionDone:
+    for i in range(n):
+        if nodes[i] == 1:
+            target = randint(0,n-1)
+            if nodes[target] == 0:
+                nodes[target] = 1
+                infectedPerLoop+=1
+        currentInfected += infectedPerLoop
+        curInf.append(currentInfected)
+        infectedPerLoop = 0
+    infectionRate.append (currentInfected)
+    attempts.append(x)
+    x+=1
+    if currentInfected == n:infectionDone = True 
 
 xpoints = np.array(attempts)
 ypoints = np.array(infectionRate)
-
-plt.plot(xpoints,ypoints)
-plt.savefig("infectionrate.jpg")
-plt.show()
-
-xpoints = np.array(attempts)
-ypoints = np.array(currentInfected)
-plt.plot(xpoints,ypoints)
-plt.savefig("infectionrate.jpg")
+print(len(attempts),len(infectionRate))
+plt.plot(xpoints, ypoints)
+plt.savefig("lab1/infectionrate.jpg")
 plt.show()
