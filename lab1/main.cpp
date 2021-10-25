d09815accaf87416c2ea0574fbe6382cc6748ec8
Theo P Charalampidis <runtheorun3@gmail.com>
2021-10-25 09:15:24 +0300

    commit on 'master' ; nothing to say

---
+++ lab1/main.cpp
@@ -0,0 +1,22 @@
+// σκοπός είναι η εξάσκηση σε c. ο κώδικας θα γράφεται σε python και περιοδικά θα γίνονται ενημερώσεις στα .c αρχεία
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+int networkSize;
+bool infectionDone = false;
+int currentInfected = 1;
+int infectedPerLoop = 0;
+
+
+int main() {
+    printf("Input number of PCs on the network:");
+    scanf("%d", &networkSize);
+    int nodes [networkSize] = {0};
+    while (infectionDone = false) {
+        for (int i = 0; i<networkSize;i++) {
+            
+        }
+    }
+        
--- lab1/main.py
+++ lab1/main.py
@@ -1,38 +1,46 @@
 from random import randint
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+
 n = 1000
 nodes = [0] * n
 infectionDone = False
 currentInfected = 1
 infectedPerLoop = 0
 infectionRate = []
+curInf = []
 attempts = []
 x=1
 
 nodes[randint(0,n-1)] = 1
 
 while infectionDone == False:
-    for i in range(n):
-        if nodes[i] == 1:
-            target = randint(0,n-1)
-            if nodes[target] == 0:
-                nodes[target] = 1
-                infectedPerLoop+=1
-        currentInfected += infectedPerLoop
-        infectionRate.append (currentInfected)
-        attempts.append(x)
-        x+=1
-        infectedPerLoop = 0
-        
-    if currentInfected == n: 
-        print(currentInfected)
-        infectionDone = True 
-        print ("Done")
+        for i in range(n):
+            if nodes[i] == 1:
+                target = randint(0,n-1)
+                if nodes[target] == 0:
+                    nodes[target] = 1
+                    infectedPerLoop+=1
+            currentInfected += infectedPerLoop
+            infectionRate.append (currentInfected)
+            curInf.append(currentInfected)
+            attempts.append(x)
+            x+=1
+            infectedPerLoop = 0
 
+        if currentInfected == n:infectionDone = True 
+            
 
 xpoints = np.array(attempts)
 ypoints = np.array(infectionRate)
+
 plt.plot(xpoints,ypoints)
+plt.savefig("infectionrate.jpg")
 plt.show()
-# plt.savefig("infectionrate.jpg")
+
+xpoints = np.array(attempts)
+ypoints = np.array(currentInfected)
+plt.plot(xpoints,ypoints)
+plt.savefig("infectionrate.jpg")
+plt.show()
